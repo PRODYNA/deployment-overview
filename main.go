2af1e0955e64685f51eca790ea622c7a7d4ae1ab
Darko Križić <darko@krizic.net>
2024-02-29 21:53:07 +0100

    More restructuring

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ func main() {
 	}
 
 	organization := &result.Organization{Name: config.Organization, Repositories: []result.Repository{}}
-	err = iterateRepositories(ctx, gh, *config, organization)
+	err = organization.IterateRepositories(ctx, gh, *config)
 	if err != nil {
 		slog.ErrorContext(ctx, "Unable to iterate repositories", "error", err)
 		return
@@ -57,90 +57,4 @@ func main() {
 
 	}
 
-}
-
-func iterateRepositories(ctx context.Context, gh *github.Client, config config.Config, organization *result.Organization) error {
-
-	// split config.repositories by comma
-	for _, rep := range config.RepositoriesAsList() {
-		slog.InfoContext(ctx, "Processing repository", "organization", config.Organization, "repository", rep)
-		repository := result.Repository{Name: rep}
-
-		repo, _, err := gh.Repositories.Get(ctx, config.Organization, rep)
-		if err != nil {
-			slog.ErrorContext(ctx, "Unable to get repository", "organization", config.Organization, "repository", rep, "error", err)
-			repository.Error = err.Error()
-			continue
-		}
-
-		// get the latest release
-		release, _, err := gh.Repositories.GetLatestRelease(ctx, config.Organization, rep)
-		if err != nil {
-			slog.ErrorContext(ctx, "Unable to get latest release", "organization", config.Organization, "repository", rep, "error", err)
-			repository.Error = err.Error()
-		} else {
-			repository.LatestRelease = result.Release{Tag: release.GetTagName(), Title: release.GetName()}
-		}
-
-		// get all releases
-		releases, _, err := gh.Repositories.ListReleases(ctx, config.Organization, rep, nil)
-		if err != nil {
-			slog.ErrorContext(ctx, "Unable to list releases", "organization", config.Organization, "repository", rep, "error", err)
-			repository.Error = err.Error()
-		} else {
-			for _, release := range releases {
-				repository.Releases = append(repository.Releases, result.Release{Tag: release.GetTagName(), Title: release.GetName()})
-			}
-		}
-
-		// check if the repository has open pull requests
-		pulls, _, err := gh.PullRequests.List(ctx, config.Organization, rep, &github.PullRequestListOptions{State: "open"})
-		if err != nil {
-			slog.ErrorContext(ctx, "Unable to list open pull requests", "organization", config.Organization, "repository", repo, "error", err)
-			repository.Error = err.Error()
-		} else {
-			repository.OpenPulls = len(pulls)
-		}
-
-		slog.DebugContext(ctx, "Repository", "organization", config.Organization, "id", repo.GetID())
-		err = iterateEnvironments(ctx, gh, config, &repository)
-		if err != nil {
-			slog.ErrorContext(ctx, "Unable to iterate environments", "organization", config.Organization, "repository", repo, "error", err)
-			repository.Error = err.Error()
-		}
-
-		organization.Repositories = append(organization.Repositories, repository)
-	}
-
-	return nil
-}
-
-func iterateEnvironments(ctx context.Context, gh *github.Client, config config.Config, repository *result.Repository) (err error) {
-	// split config.environments by comma
-	for _, env := range config.EnvironmentsAsList() {
-		slog.InfoContext(ctx, "Processing environment", "organization", config.Organization, "repository", repository.Name, "environment", env)
-		environment := result.Environment{Name: env}
-
-		deployments, _, err := gh.Repositories.ListDeployments(ctx, config.Organization, repository.Name, nil)
-		if err != nil {
-			slog.ErrorContext(ctx, "Unable to list deployments", "organization", config.Organization, "repository", repository.Name, "error", err)
-			environment.Version = err.Error()
-			continue
-		}
-
-		for _, deployment := range deployments {
-			if deployment.Environment != nil && *deployment.Environment == env {
-				ref := deployment.GetRef()
-				if ref == "master" || ref == "main" {
-					environment.Version = deployment.GetSHA()[0:7]
-				} else {
-					environment.Version = deployment.GetRef()
-				}
-				repository.Environments = append(repository.Environments, environment)
-				break
-			}
-		}
-	}
-
-	return nil
 }
--- result/result.go
+++ result/result.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"github.com/google/go-github/v59/github"
+	"github.com/prodyna/deployment-overview/config"
 	"log/slog"
 	"os"
 	"text/template"
@@ -18,10 +20,10 @@ var markdownTemplate = `
 {{if .Error}}
 Error: {{.Error}}
 {{else}}
-Latest Tag: {{.LatestTag}}
-Commits After Tag: {{.CommitsAfterTag}}
-Latest Release: {{.LatestRelease.Tag}} - {{.LatestRelease.Title}}
-Open Pulls: {{.OpenPulls}}
+- Latest Tag: {{.LatestTag}}
+- Commits After Tag: {{.CommitsAfterTag}}
+- Latest Release: {{.LatestRelease.Tag}} - {{.LatestRelease.Title}}
+- Open Pulls: {{.OpenPulls}}
 
 ### Environments
 {{range .Environments}}
@@ -55,6 +57,92 @@ type Repository struct {
 type Environment struct {
 	Name    string `json:"name"`
 	Version string `json:"version"`
+}
+
+func (organization *Organization) IterateRepositories(ctx context.Context, gh *github.Client, config config.Config) error {
+
+	// split config.repositories by comma
+	for _, rep := range config.RepositoriesAsList() {
+		slog.InfoContext(ctx, "Processing repository", "organization", config.Organization, "repository", rep)
+		repository := Repository{Name: rep}
+
+		repo, _, err := gh.Repositories.Get(ctx, config.Organization, rep)
+		if err != nil {
+			slog.ErrorContext(ctx, "Unable to get repository", "organization", config.Organization, "repository", rep, "error", err)
+			repository.Error = err.Error()
+			continue
+		}
+
+		// get the latest release
+		release, _, err := gh.Repositories.GetLatestRelease(ctx, config.Organization, rep)
+		if err != nil {
+			slog.ErrorContext(ctx, "Unable to get latest release", "organization", config.Organization, "repository", rep, "error", err)
+			repository.Error = err.Error()
+		} else {
+			repository.LatestRelease = Release{Tag: release.GetTagName(), Title: release.GetName()}
+		}
+
+		// get all releases
+		releases, _, err := gh.Repositories.ListReleases(ctx, config.Organization, rep, nil)
+		if err != nil {
+			slog.ErrorContext(ctx, "Unable to list releases", "organization", config.Organization, "repository", rep, "error", err)
+			repository.Error = err.Error()
+		} else {
+			for _, release := range releases {
+				repository.Releases = append(repository.Releases, Release{Tag: release.GetTagName(), Title: release.GetName()})
+			}
+		}
+
+		// check if the repository has open pull requests
+		pulls, _, err := gh.PullRequests.List(ctx, config.Organization, rep, &github.PullRequestListOptions{State: "open"})
+		if err != nil {
+			slog.ErrorContext(ctx, "Unable to list open pull requests", "organization", config.Organization, "repository", repo, "error", err)
+			repository.Error = err.Error()
+		} else {
+			repository.OpenPulls = len(pulls)
+		}
+
+		slog.DebugContext(ctx, "Repository", "organization", config.Organization, "id", repo.GetID())
+		err = repository.IterateEnvironments(ctx, gh, config)
+		if err != nil {
+			slog.ErrorContext(ctx, "Unable to iterate environments", "organization", config.Organization, "repository", repo, "error", err)
+			repository.Error = err.Error()
+		}
+
+		organization.Repositories = append(organization.Repositories, repository)
+	}
+
+	return nil
+}
+
+func (repository *Repository) IterateEnvironments(ctx context.Context, gh *github.Client, config config.Config) (err error) {
+	// split config.environments by comma
+	for _, env := range config.EnvironmentsAsList() {
+		slog.InfoContext(ctx, "Processing environment", "organization", config.Organization, "repository", repository.Name, "environment", env)
+		environment := Environment{Name: env}
+
+		deployments, _, err := gh.Repositories.ListDeployments(ctx, config.Organization, repository.Name, nil)
+		if err != nil {
+			slog.ErrorContext(ctx, "Unable to list deployments", "organization", config.Organization, "repository", repository.Name, "error", err)
+			environment.Version = err.Error()
+			continue
+		}
+
+		for _, deployment := range deployments {
+			if deployment.Environment != nil && *deployment.Environment == env {
+				ref := deployment.GetRef()
+				if ref == "master" || ref == "main" {
+					environment.Version = deployment.GetSHA()[0:7]
+				} else {
+					environment.Version = deployment.GetRef()
+				}
+				repository.Environments = append(repository.Environments, environment)
+				break
+			}
+		}
+	}
+
+	return nil
 }
 
 func (organization *Organization) RenderJson(ctx context.Context) (result []byte, err error) {
