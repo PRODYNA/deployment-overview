2930bf095b25cbc3d0d97bd04ef51a1703188a2e
Darko Križić <darko@krizic.net>
2024-03-02 17:02:10 +0100

    Error return code

--- main.go
+++ main.go
@@ -13,29 +13,37 @@ import (
 )
 
 func main() {
+	err := do()
+	if err != nil {
+		os.Exit(1)
+	}
+	os.Exit(0)
+}
+
+func do() error {
 	ctx := context.Background()
 	c, err := config.CreateConfig(ctx)
 	if err != nil {
 		slog.ErrorContext(ctx, "Unable to create c", "error", err)
-		return
+		return err
 	}
 	err = c.Validate()
 	if err != nil {
 		slog.ErrorContext(ctx, "Invalid c", "error", err)
-		return
+		return err
 	}
 
 	// try to load the template file
 	template, err := os.ReadFile(c.TemplateFile)
 	if err != nil {
 		slog.ErrorContext(ctx, "Unable to read template file", "error", err, "file", c.TemplateFile)
-		return
+		return err
 	}
 
 	gh := github.NewClient(nil).WithAuthToken(c.GithubToken)
 	if gh == nil {
 		slog.ErrorContext(ctx, "Unable to create github client")
-		return
+		return err
 	}
 
 	organization := &result.Organization{Title: c.Title, Repositories: []result.Repository{}}
@@ -43,22 +51,27 @@ func main() {
 	err = organization.IterateRepositories(ctx, gh, *c)
 	if err != nil {
 		slog.ErrorContext(ctx, "Unable to iterate repositories", "error", err)
-		return
+		return err
 	}
 
 	json, err := organization.RenderJson(ctx)
 	if err != nil {
 		slog.ErrorContext(ctx, "Unable to render organization", "error", err)
+		return err
 	}
 	fmt.Printf("%s\n", json)
 
 	md, err := organization.RenderMarkdown(ctx, string(template))
 	if err != nil {
 		slog.ErrorContext(ctx, "Unable to render organization", "error", err)
+		return err
 	}
 
 	err = publish.PublishToGitHub(ctx, c, md, gh)
 	if err != nil {
 		slog.ErrorContext(ctx, "Unable to publish to github", "error", err)
+		return err
 	}
+
+	return nil
 }
